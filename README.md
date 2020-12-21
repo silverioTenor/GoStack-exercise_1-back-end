76b5befabaedd9ce751c832f04cb192634aacc4b
Rodrigo Silvério <silverio_tenor@outlook.com>
2020-12-21 18:28:33 -0300

    Primeiro commit. ;)

---
+++ README.md
@@ -0,0 +1,9 @@
+# API com Node.js
+
+Neste exercício foi implementada uma API para criação, exibição, listagem, atualização e 
+remoção de filmes.
+
+Os pacotes usados neste exercício foram:
+
+* [express](https://www.npmjs.com/package/express) => para fins de roteamento;
+* [uuid](https://www.npmjs.com/package/uuid) => para identificação dos dados manipulados.
---
+++ src/index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const { v4: uuidv4, validate: isUuid } = require('uuid');
+
+const app = express();
+
+app.use(express.json());
+
+const movies = [];
+
+function idValidate(req, res, next) {
+  const { id } = req.params;
+
+  if (!isUuid(id)) return res.status(400).json({ error: "Invalid movie ID!" });
+
+  return next();
+}
+
+app.use('/movies/:id', idValidate);
+
+app.get("/movies", (req, res) => {
+  return res.json(movies);
+});
+
+app.post("/movies", (req, res) => {
+  const { title, year } = req.body;
+
+  const movie = { id: uuidv4(), title, year };
+
+  movies.push(movie);
+
+  return res.json(movie);
+});
+
+app.put("/movies/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, year } = req.body;
+
+  const index = movies.findIndex(movie => movie.id === id);
+
+  if (index < 0) return res.status(400).json({ error: "Movie not found!" });
+
+  const movie = { id, title, year };
+
+  movies[index] = movie;
+
+  return res.json(movie);
+});
+
+app.delete("/movies/:id", (req, res) => {
+  const { id } = req.params;
+
+  const index = movies.findIndex(movie => movie.id === id);
+
+  if (index < 0) return res.status(400).json({ error: "Movie not found!" });
+
+  movies.splice(index, 1);
+
+  return res.status(204).send();
+});
+
+app.listen(3333, () => console.log("Server is running..."));
